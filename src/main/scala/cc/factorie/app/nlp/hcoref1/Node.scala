45e2deeaf179d593864f3fd65f6b7f80a6439ed5
Harshal Pandya <harshalgem@gmail.com>
2013-10-08 18:04:55 -0400

    Does not compile yet

--- src/main/scala/cc/factorie/app/nlp/hcoref1/Node.scala
+++ src/main/scala/cc/factorie/app/nlp/hcoref1/Node.scala
@@ -9,20 +9,33 @@ import cc.factorie.app.nlp.hcoref.SparseBagOfWords
  * @date: 10/3/13
  */
 
-abstract class Node[T <: Node[T]] {
+trait Node[T <: Node[T,L], L <: LeafNode[L]] {
   this: T =>
   val parentRef = new ArrowVariable[T, Option[T]](this, None)
-  def parent : Option[T] = parentRef.dst
-  def children : Seq[T]
-  def descendants : Seq[T] //dfs or bfs
-  def leaves : Seq[T] = descendants.filter(_.children.isEmpty)
-  private def addChild(c: T){
-
+  def parent: Option[T] = parentRef.dst
+  var _children: Seq[Node[_,_]] = _
+  def children: Seq[Node[_,_]] = _children
+  def descendants: Seq[T] = {
+    def helper(n:T): Seq[T] = {
+      if(n.children.isEmpty) Seq(n)
+      n.children.foldLeft(Seq.empty[T])((seq,n)=> seq ++ helper(n))
+    }
+    helper(this)
   }
-  @tailrec
-  final def root : T = parent match { case Some(p) => p.root; case None => this }
+  def leaves: Seq[L] = descendants.flatMap {
+      case leaf: L => Some(leaf)
+      case _ => None
+  }
+  def addChild(c: Node[_,_]){
+    _children = _children :+ c
+  }
 
-  def depth : Int = {
+  @tailrec
+  final def root: T = parent match { case Some(p) => p.root; case None => this }
+
+  final def isRoot: Boolean = !this.parent.isDefined
+
+  final def depth: Int = {
     def depthHelper(root: Option[T]):Int = {
       if(!parent.isDefined) 0
       else 1 + depthHelper(parent)
@@ -30,7 +43,7 @@ abstract class Node[T <: Node[T]] {
     depthHelper(Some(this))
   }
 
-  def size : Int  = ???
+  def size: Int = descendants.size
 
   def alterParent(p:Option[T]){
     val oldParent = parent
@@ -43,22 +56,21 @@ abstract class Node[T <: Node[T]] {
     }
   }
 
-  def bow[B <: SparseBagOfWords]()
-
   def alter(a:Any):PartialFunction[Any, Int] = {
     case a:Int => 1
   }
 }
 
 
-//class Mention extends Node[Mention] {
-//  val children = Nil
-//  override val size = 1
-//  val descendants = Nil
-//  override val leaves = Seq(this)
-//
-//  override def alter(a:Any):PartialFunction[Any, Int] = super.alter(a) orElse {
-//    case _ =>  2
-//  }
-//
-//}
+trait LeafNode[T <: LeafNode[T]] extends Node[LeafNode[T], T] {
+  override val children = Nil
+  override val size = 1
+  override val descendants = Nil
+  override val leaves = Seq(this)
+}
+
+class Mention extends LeafNode[Mention]
+
+class InternalNode extends Node[InternalNode, Mention]
+
+
