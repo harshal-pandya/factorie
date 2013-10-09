5e6f25382dca34ee6c00719b69d8df7dc6f4c9e0
John Sullivan <john.t.sullivan@gmail.com>
2013-10-09 11:15:51 -0400

    removed old node implementation

--- src/main/scala/cc/factorie/app/nlp/hcoref1/Node.scala
+++ src/main/scala/cc/factorie/app/nlp/hcoref1/Node.scala
@@ -1,14 +1,13 @@
 package cc.factorie.app.nlp.hcoref1
 
-import cc.factorie.variable.ArrowVariable
 import scala.annotation.tailrec
 import scala.collection.mutable
+import cc.factorie.variable.ArrowVariable
 
 /**
- * @author harshal
+ * @author harshal, Jack Sullivan
  * @date: 10/3/13
  */
-
 abstract class Node[Vars <: NodeVariables[Vars]](private val variables:Vars) {
 
   type This = Node[Vars]
@@ -43,6 +42,7 @@ abstract class Node[Vars <: NodeVariables[Vars]](private val variables:Vars) {
     }
     helper(parent,0)
   }
+  final def isEntity:Boolean = !parent.isDefined
 
   //todo do we even need these?
   protected def leavingParent(parent:This)
@@ -82,71 +82,3 @@ abstract class Mention[Vars <: NodeVariables[Vars]](v:Vars) extends Node[Vars](v
   override final val descendants = Nil
   override final val leaves = this :: Nil
 }
-
-
-/*
-trait Node[T <: Node[T,L], L <: LeafNode[L]] {
-  this: T =>
-  val parentRef = new ArrowVariable[T, Option[T]](this, None)
-  def parent: Option[T] = parentRef.dst
-  var _children: Seq[Node[_,_]] = _
-  def children: Seq[Node[_,_]] = _children
-  def descendants: Seq[T] = {
-    def helper(n:T): Seq[T] = {
-      if(n.children.isEmpty) Seq(n)
-      n.children.foldLeft(Seq.empty[T])((seq,n)=> seq ++ helper(n))
-    }
-    helper(this)
-  }
-  def leaves: Seq[L] = descendants.flatMap {
-      case leaf: L => Some(leaf)
-      case _ => None
-  }
-  def addChild(c: Node[_,_]){
-    _children = _children :+ c
-  }
-
-  @tailrec
-  final def root: T = parent match { case Some(p) => p.root; case None => this }
-
-  final def isRoot: Boolean = !this.parent.isDefined
-
-  final def depth: Int = {
-    def depthHelper(root: Option[T]):Int = {
-      if(!parent.isDefined) 0
-      else 1 + depthHelper(parent)
-    }
-    depthHelper(Some(this))
-  }
-
-  def size: Int = descendants.size
-
-  def alterParent(p:Option[T]){
-    val oldParent = parent
-    val newParent = p.get
-    //call hook
-    p match{
-      case Some(parent) => {
-        newParent.addChild(this)
-      }
-    }
-  }
-
-  def alter(a:Any):PartialFunction[Any, Int] = {
-    case a:Int => 1
-  }
-}
-
-
-trait LeafNode[T <: LeafNode[T]] extends Node[LeafNode[T], T] {
-  override val children = Nil
-  override val size = 1
-  override val descendants = Nil
-  override val leaves = Seq(this)
-}
-
-class Mention extends LeafNode[Mention]
-
-class InternalNode extends Node[InternalNode, Mention]
-
- */
