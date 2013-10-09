663e722fe48900b8ab2f4f23e9bb35b1329ef537
John Sullivan <john.t.sullivan@gmail.com>
2013-10-09 02:57:07 -0400

    New rewrite proposal for Node and friends.

--- src/main/scala/cc/factorie/app/nlp/hcoref1/Node.scala
+++ src/main/scala/cc/factorie/app/nlp/hcoref1/Node.scala
@@ -2,13 +2,89 @@ package cc.factorie.app.nlp.hcoref1
 
 import cc.factorie.variable.ArrowVariable
 import scala.annotation.tailrec
-import cc.factorie.app.nlp.hcoref.SparseBagOfWords
+import scala.collection.mutable
 
 /**
  * @author harshal
  * @date: 10/3/13
  */
 
+abstract class Node[Vars <: NodeVariables[Vars]](private val variables:Vars) {
+
+  type This = Node[Vars]
+  type ThisMention = Mention[Vars]
+
+  private val parentRef = new ArrowVariable[This, This](this, null)
+  protected val _children:mutable.HashSet[This] = mutable.HashSet[This]()
+
+  final def parent:Option[This] = Option(parentRef.dst)
+  def children:Iterable[This] = _children
+  def descendants:Iterable[This] = if(children.nonEmpty) { //todo make tailrec
+    children.flatMap{_.descendants}
+  } else {
+    Nil
+  }
+  def leaves:Iterable[ThisMention] = descendants.collect{case desc:ThisMention => desc}
+
+  @tailrec
+  final def root:This = parent match {
+    case Some(p) => p.root
+    case None => this
+  }
+
+  final def size:Int = descendants.size
+  final def depth:Int = {
+    @tailrec
+    def helper(pOpt:Option[This], cDepth:Int):Int = {
+      pOpt match {
+        case Some(p) => helper(p.parent, cDepth + 1)
+        case None => cDepth
+      }
+    }
+    helper(parent,0)
+  }
+
+  //todo do we even need these?
+  protected def leavingParent(parent:This)
+  protected def joiningParent(parent:This)
+
+  final def alterParent(newParent:Option[This]) {
+    val oldParent = this.parent
+    oldParent match {
+      case Some(oParent) => {
+        leavingParent(oParent)
+        oParent.variables remove this.variables
+        oParent._children -= this
+      }
+      case None => Unit
+    }
+    newParent match {
+      case Some(nParent) => {
+        joiningParent(nParent)
+        nParent.variables combine this.variables
+        parentRef.set(nParent)(null) // todo we probably do want a difflist here
+        nParent._children += this
+      }
+      case None => parentRef.set(null)(null) // todo we probably do want a difflist here
+    }
+  }
+}
+
+trait NodeVariables[Self <: NodeVariables[Self]] {
+  this :Self =>
+
+  def combine(other:Self):Self
+  def remove(other:Self):Self
+}
+
+abstract class Mention[Vars <: NodeVariables[Vars]](v:Vars) extends Node[Vars](v) {
+  override final val children = Nil
+  override final val descendants = Nil
+  override final val leaves = this :: Nil
+}
+
+
+/*
 trait Node[T <: Node[T,L], L <: LeafNode[L]] {
   this: T =>
   val parentRef = new ArrowVariable[T, Option[T]](this, None)
@@ -73,4 +149,4 @@ class Mention extends LeafNode[Mention]
 
 class InternalNode extends Node[InternalNode, Mention]
 
-
+ */
